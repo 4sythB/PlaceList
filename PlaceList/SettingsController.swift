d5fe26018557ca09c9a407eee1ebd2bcb2b78029
Brad Forsyth <4sythb@gmail.com>
2016-09-14 12:05:26 -0600

    Added a settings controller and lowered deployment

---
+++ PlaceList/SettingsController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsController.swift
+//  PlaceList
+//
+//  Created by Brad on 9/14/16.
+//  Copyright © 2016 Brad Forsyth. All rights reserved.
+//
+
+import Foundation
+
+class SettingsController {
+    
+    var theme: appearanceTheme = .darkTheme
+    
+    enum appearanceTheme {
+        case lightTheme
+        case darkTheme
+    }
+    
+    
+}
