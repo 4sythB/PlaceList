31e405c2362b22f171669c6d44248aff505c26c4
Brad Forsyth <4sythb@gmail.com>
2016-09-14 22:03:26 -0600

    Customized appearance of SettingsViewController

--- PlaceList/SettingsViewController.swift
+++ PlaceList/SettingsViewController.swift
@@ -12,14 +12,31 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var doneButton: UIBarButtonItem!
     @IBOutlet weak var darkModeSwitch: UISwitch!
+    @IBOutlet weak var themeHeadingLabel: UILabel!
+    @IBOutlet weak var themeModeLabel: UILabel!
+    @IBOutlet weak var mapTypeHeadingLabel: UILabel!
+    @IBOutlet weak var mapTypeSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        darkModeSwitch.onTintColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+        darkModeSwitch.tintColor = UIColor(red:0.40, green:0.41, blue:0.43, alpha:1.00)
+        
         if SettingsController.sharedController.theme == .darkTheme {
             darkModeSwitch.on = true
+            self.view.backgroundColor = UIColor(red:0.19, green:0.20, blue:0.23, alpha:1.00)
+            themeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            themeModeLabel.textColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00)
+            mapTypeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            mapTypeSegmentedControl.tintColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00)
         } else if SettingsController.sharedController.theme == .lightTheme {
             darkModeSwitch.on = false
+            self.view.backgroundColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00)
+            themeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            themeModeLabel.textColor = UIColor(red:0.19, green:0.20, blue:0.23, alpha:1.00)
+            mapTypeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            mapTypeSegmentedControl.tintColor = UIColor(red:0.19, green:0.20, blue:0.23, alpha:1.00)
         }
     }
     
@@ -32,9 +49,23 @@ class SettingsViewController: UIViewController {
     @IBAction func darkModeSwitchChanged(sender: UISwitch) {
         if sender.on {
             SettingsController.sharedController.theme = .darkTheme
+            self.view.backgroundColor = UIColor(red:0.19, green:0.20, blue:0.23, alpha:1.00)
+            themeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            themeModeLabel.textColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00)
+            mapTypeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            mapTypeSegmentedControl.tintColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00)
         } else {
             SettingsController.sharedController.theme = .lightTheme
+            self.view.backgroundColor = UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00)
+            themeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            themeModeLabel.textColor = UIColor(red:0.19, green:0.20, blue:0.23, alpha:1.00)
+            mapTypeHeadingLabel.textColor = UIColor(red:0.42, green:0.66, blue:0.76, alpha:1.00)
+            mapTypeSegmentedControl.tintColor = UIColor(red:0.19, green:0.20, blue:0.23, alpha:1.00)
         }
+    }
+    
+    @IBAction func mapTypeControlChanged(sender: AnyObject) {
+        
     }
     
     
